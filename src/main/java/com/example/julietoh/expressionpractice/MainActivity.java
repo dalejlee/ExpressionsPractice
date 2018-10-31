199176e42b2efc556f290b6e412318e42c603048
Juliet Oh <julietoh@Juliets-MacBook-Pro.local>
2018-10-31 01:44:27 -0700

    Ask for camera permission

--- src/main/java/com/example/julietoh/expressionpractice/MainActivity.java
+++ src/main/java/com/example/julietoh/expressionpractice/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.julietoh.expressionpractice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_PERMISSIONS_REQUEST = 42;  //value is arbitrary (between 0 and 255)
+    boolean cameraPermissionsAvailable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkForCameraPermissions();
+    }
+
+    private void checkForCameraPermissions() {
+        cameraPermissionsAvailable =
+                ContextCompat.checkSelfPermission(
+                        getApplicationContext(),
+                        Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+
+        if (!cameraPermissionsAvailable) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                // showPermissionExplanationDialog(CAMERA_PERMISSIONS_REQUEST);
+            } else {
+                // No explanation needed, we can request the permission.
+                requestCameraPermissions();
+            }
+        }
+    }
+
+    private void requestCameraPermissions() {
+        if (!cameraPermissionsAvailable) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CAMERA},
+                    CAMERA_PERMISSIONS_REQUEST);
+
+            // CAMERA_PERMISSIONS_REQUEST is an app-defined int constant that must be between 0 and 255.
+            // The callback method gets the result of the request.
+        }
     }
 }
