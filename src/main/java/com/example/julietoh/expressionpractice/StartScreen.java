626a4f4d644d2f713724638e84e8abb8849afd9a
dalejlee <dalelee@princeton.edu>
2018-11-23 02:43:30 -0500

    added drawables and emotion quiz files

---
+++ src/main/java/com/example/julietoh/expressionpractice/StartScreen.java
@@ -0,0 +1,30 @@
+package com.example.julietoh.expressionpractice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+
+        Button startQuizButton = findViewById(R.id.button_start_quiz);
+
+        startQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startQuiz();
+            }
+        });
+
+    }
+    private void startQuiz() {
+        Intent intent = new Intent(StartScreen.this, QuizActivity.class);
+        startActivity(intent);
+    }
+}
