6b94c804e0add70691f32017c35152325efdbbb4
Juliet Oh <julietoh@nat-oitwireless-inside-vapornet100-d-5575.Princeton.EDU>
2018-11-28 06:54:16 -0500

    Tune emotions

--- src/main/java/com/example/julietoh/expressionpractice/ResultActivity.java
+++ src/main/java/com/example/julietoh/expressionpractice/ResultActivity.java
@@ -1,6 +1,7 @@
 package com.example.julietoh.expressionpractice;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -15,6 +16,14 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
         TextView resultTextView = findViewById(R.id.result_text);
         resultTextView.setText("Correct! \n You expressed joy");
+        CountDownTimer CountDownTimer = new CountDownTimer(10000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                // do nothing
+            }
+            public void onFinish() {
+                finish();
+            }
+        }.start();
     }
 
 }
